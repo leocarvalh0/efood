0ee9c98d5232655ffcd35c73153d17370cc20063
leocarvalh0 <0leonardocarvalho@gmail.com>
2026-03-17 06:55:39 -0300

    arrumando um erro no container Cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -15,7 +15,7 @@ const Cart = () => {
 
   const getTotalPrices = () => {
     return items.reduce((acumulador, valorAtual) => {
-      return (acumulador += valorAtual.preco!)
+      return acumulador + (valorAtual.preco || 0)
     }, 0)
   }
 
